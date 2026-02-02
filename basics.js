ddcbb75a924f901c047226cd73ef79f2697c9709
unknown <shahzaibjoiya572@gmail.com>
2026-02-02 14:26:54 +0500

    fun,obj,stats,loops,array added

--- basics.js
+++ basics.js
@@ -4,10 +4,37 @@ const c = 67;
 a = 90;
 // c = 10;
 console.log(a+b+c)
-// integrating loops
+// integrating conditios statements
 if (a==34 || a==90){
     console.log("this is true")
 }
 else {
     console.log("this is not true")
 }
+//integrating  FOR loops
+
+// for (var a=0 ;a<=10; a++)
+//     console.log(a)
+// integrating while loops
+
+// var a=0;
+// while(a<=20){
+//     console.log(a)
+// a++;
+// }
+
+// integrating array
+
+// var names =["Usman", "Ali Imran", "M Mushtaq", "Muhammad Basit"]
+// // console.log(names[3])
+// for (var a=0;a<names.length;a++)
+//     console.log(names[a])
+
+// integrating objects
+
+var user={
+    name: "Shahzaib",
+    city: "Lahore",
+    age: 23,
+}
+console.log(user.name)
