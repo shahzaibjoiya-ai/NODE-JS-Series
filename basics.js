ed075fab11840746c2d8cb9c7c3cb5411f45f4fd
unknown <shahzaibjoiya572@gmail.com>
2026-02-02 13:55:56 +0500

    fun,loops & obj are added

---
+++ basics.js
@@ -0,0 +1,13 @@
+var a = 34;
+let b = 78;
+const c = 67;
+a = 90;
+// c = 10;
+console.log(a+b+c)
+// integrating loops
+if (a==34 || a==90){
+    console.log("this is true")
+}
+else {
+    console.log("this is not true")
+}
