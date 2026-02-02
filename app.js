be54fd6d9d49ddd643a9281697d2e7872a974023
unknown <shahzaibjoiya572@gmail.com>
2026-02-02 11:55:21 +0500

    file system added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-console.log(30+30);
-console.log(50-20);
-function fruit(item){
-    console.log("fruit name is "+item)
-}
-fruit("apple")
-var a = 20
-var b = 60
-console.log(a+b)
+var fs = require ("fs") //fs ->file system
+fs.writeFileSync("Shahzaib Asghar.txt" ,"My Name Is Shahzaib Asghar And I am doing Node Js")
+// console.log(30+30);
+// console.log(50-20);
+// function fruit(item){
+//     console.log("fruit name is "+item)
+// }
+// fruit("apple")
+// var a = 20
+// var b = 60
+// console.log(a+b)
