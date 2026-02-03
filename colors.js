66f480461d06eecf02a52fa5ae61f0f4901d37d2
unknown <shahzaibjoiya572@gmail.com>
2026-02-03 16:37:51 +0500

    use of installing Externel Packages

---
+++ colors.js
@@ -0,0 +1,5 @@
+const colors = require ("colors")
+console.log(colors.green("Hi this shahzaib asghar"));
+console.log(colors.red("Hi this shahzaib asghar"));
+console.log(colors.yellow("Hi this shahzaib asghar"));
+console.log(colors.gray("Hi this shahzaib asghar"));
