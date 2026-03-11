c5b37cbd81fc2e4d8eaf82ee6921c2232f18a499
unknown <shahzaibjoiya572@gmail.com>
2026-03-11 15:40:42 +0500

    done with 404 page

---
+++ 404.js
@@ -0,0 +1,24 @@
+// ES import and export methods
+import express from "express";
+import homePage from "./pages/home.js";
+import aboutPage from "./pages/about.js";
+import {contactPage} from "./pages/home.js";
+import path from "path";
+const app = express();
+app.get('/', (req, res) => {
+  res.send(homePage());
+});
+app.get('/about', (req, res) => {
+  res.send(aboutPage());
+});
+app.get('/contact', (req, res) => {
+  res.send(contactPage());
+});
+app.use((req, res) => {
+  const absPath = path.resolve("pages/404.html");
+  res.status(404).sendFile(absPath);
+});
+const port = 3200;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
